2e16fa5862acdfc4b33983d17224fd7a45e7469b
Aurore Langlet <aurore_langlet@etik.com>
2024-10-01 20:19:36 -0400

    Sous-dossiers components pour éviter bordel

---
+++ src/lib/components/notifications/notifSuccess.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let success
+</script>
+
+<div class="container has-text-centered">
+    {#if success}
+    <div class="notification is-success">
+        <button class="delete"></button>
+        <p>{success}</p>
+    </div>
+    {/if}
+</div>
