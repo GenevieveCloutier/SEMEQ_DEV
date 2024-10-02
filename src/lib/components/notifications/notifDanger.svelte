45fe587143d2608351d0747883152b988bb7ed84
Aurore Langlet <aurore_langlet@etik.com>
2024-10-01 20:46:09 -0400

    Suppression notification fonctionnelle

--- src/lib/components/notifications/notifDanger.svelte
+++ src/lib/components/notifications/notifDanger.svelte
@@ -1,10 +1,15 @@
 <script>
     export let erreur
+
+  function supprimerNotification(event) {
+    event.target.parentNode.remove();
+  }
 </script>
 
-<div class="container has-text-centered">
+<div class="block">
     {#if erreur}
-    <div class="notification is-danger">
+    <div class="notification is-danger has-text-centered">
+        <button class="delete" on:click={supprimerNotification}></button>
         <p>{erreur}</p>
     </div>
     {/if}
