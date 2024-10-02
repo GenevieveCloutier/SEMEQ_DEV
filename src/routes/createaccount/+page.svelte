ec88d36afde7aaac22246cc7961069b8882f0fed
MapleCialys <160977634+MapleCialys@users.noreply.github.com>
2024-10-02 19:48:01 -0400

    forcage du theme clair

--- src/routes/createaccount/+page.svelte
+++ src/routes/createaccount/+page.svelte
@@ -1,13 +1,5 @@
-<script>
-import { nouveauCompte } from '../../lib/outils/formHandlers';
-</script>
+
 
 <h1 class="title">Creer un compte</h1>
-<div class="section">
-    <form on:submit|preventDefault={nouveauCompte}>
-        <input type="text" name="courriel" id="courriel"><br>
-        <input type="text" name="password" id="password"><br>
-        <button type="submit" class="button">Valider</button>
-    </form>
-</div>
+
 <a href="createaccount/exposant" class="button"> compte exposant</a>
