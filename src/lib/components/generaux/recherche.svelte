37cb9970da13204ee74549238ce9f7ccefaabaf7
Aurore Langlet <aurore_langlet@etik.com>
2024-10-01 22:36:21 -0400

    Recherche préparatifs

---
+++ src/lib/components/generaux/recherche.svelte
@@ -0,0 +1,10 @@
+<script>
+    let searchQuery = '';
+    export let typeRecherche
+</script>
+
+<div class="field">
+    <div class="control">
+        <input class="input" type="text" name="recherche" placeholder="Rechercher un {typeRecherche}..." bind:value={searchQuery} />
+    </div>
+</div>
---
+++ src/lib/components/generaux/rechercheNoResult.svelte
@@ -0,0 +1,17 @@
+<script>
+    let searchQuery = '';
+</script>
+
+<div class="no-results">
+    <p>Désolé, aucun résultat n'a été trouvé pour "{searchQuery}".</p>
+</div>
+
+<style>
+.no-results{
+    width: 100%;
+    display: flex;
+    padding: 20px;
+    font-size: 1.5rem;
+    background-color: lightgray;
+}
+</style>
