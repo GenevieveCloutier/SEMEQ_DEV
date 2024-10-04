d6dc165f9cb48666ef2087f1ff4e88631d0b09c2
Aurore Langlet <aurore_langlet@etik.com>
2024-10-04 19:44:19 -0400

    autres conditions (dates toujours problèmes)

--- src/routes/appels_candidatures/+page.server.js
+++ src/routes/appels_candidatures/+page.server.js
@@ -2,24 +2,26 @@ import { Op } from "sequelize";
 import { findAll } from "$lib/db/controllers/Evenements.controller.js";
 
 /**
- * Charge les détails des événements approuvés dont la date d'aujourd'hui est comprise entre la date de début et
- * celle de fin des appels de candidatures des événements du répertoire.
- * Si pas de date de début de candidature dès que l'événement est approuvé.
- * Si pas de date de fin de candidature, retirer dès la date de début de l'événement.
+ * Charge les détails des événements approuvés dont la date de fin des appels de candidatures est après la date du jour.
+ * Si pas de dates de candidature, ne pas afficher à partir de la date de début de l'événement.
  * @param {Object} params - Les paramètres de la requête.
  * @returns {Object} - Les événements et leurs détails pour ceux ayant des appels de candidatures en cours
  *                     (si utilisateur exposant abonné connecté).
  */
 export async function load({ params }){
-    const aujourdhui = new Date().toISOString().split('T')[0];  // date du jour au format ISO (AAAA-MM-JJ) sans l'heure
+    //const aujourdhui = new Date().toISOString().split('T')[0];  // date du jour au format ISO sans l'heure
+
     const events = await findAll({
         where: {
-            debut_cand: {
-                [Op.lte]: aujourdhui // debut_cand inférieur ou égal à date du jour
-            },
-            fin_cand: {
-                [Op.gte]: aujourdhui // fin_cand supérieur ou égal à date du jour
-            }
+            // événement approuvé
+            approuve: true,
+            // fin_cand a une date inférieure (avant) à date du jour ou fin_cand est null
+            [Op.or]: [
+                { fin_cand: { [Op.lt]: new Date() } },
+                { fin_cand: null }
+            ],
+            // début_even a une date inférieure (avant) ou égale à date du jour
+            debut_even: { [Op.lte]: new Date() }
         }
     });
 
