fdf3647155ad67959e60e5c99ff9e2ccdd8a2ec1
Aurore Langlet <aurore_langlet@etik.com>
2024-10-03 23:11:11 -0400

    Améliorations et tests débutés

--- src/routes/appels_candidatures/+page.server.js
+++ src/routes/appels_candidatures/+page.server.js
@@ -1,20 +1,16 @@
 import { findAll } from "$lib/db/controllers/Evenements.controller.js";
-import { findAll as findVilles, findOne as findNomVille } from "$lib/db/controllers/Villes.controller.js";
 
 /**
  * Charge les détails des événements approuvés dont la date d'aujourd'hui est comprise entre la date de début et
  * celle de fin des appels de candidatures des événements du répertoire.
+ * Si pas de date de début de candidature dès que l'événement est approuvé.
+ * Si pas de date de fin de candidature, retirer dès la date de début de l'événement.
  * @param {Object} params - Les paramètres de la requête.
  * @returns {Object} - Les événements et leurs détails pour ceux ayant des appels de candidatures en cours
  *                     (si utilisateur exposant abonné connecté).
  */
 export async function load({ params }){
     const events = await findAll();
-    const villes = await findVilles();
-    let ville = null;
-    for (const event of villes) {
-        ville = await findNomVille({id: event.ville_id});
-        event.ville = ville;
-    }
+
     return { events:events }
 }
--- src/routes/appels_candidatures/+page.svelte
+++ src/routes/appels_candidatures/+page.svelte
@@ -29,19 +29,19 @@
             {#each events as event}
             <tr>
                 <td>{event.nom || "Inconnu"}</td>
-                <td></td>
-                <td>{event.ville.nom || "Inconnu"}</td>
+                <td>{event.ville.region.nom || "Inconnue"}</td>
+                <td>{event.ville.nom || "Inconnue"}</td>
                 <td>{event.debut_even || "Inconnu"}</td>
                 <td>{event.fin_even || "Inconnu"}</td>
                 <td>
                     {#if event.form_cand}   <!-- Formulaire appel de candidatures -->
-                        <a href="{event.form_cand}" class="button" style="background-color: #053682; color:white">Postuler</a>
+                        <a href="{event.form_cand}" target="_blank" class="button" style="background-color: #053682; color:white">Postuler</a>
                     {:else if event.courriel}   <!-- Courriel pour information ou inscription -->
                         <a href="mailto:{event.courriel}" class="button" style="background-color: #053682; color:white">Postuler</a>
                     {:else if event.fb_even}    <!-- Événement Facebook -->
-                        <a href="{event.fb_even}" class="button" style="background-color: #053682; color:white">Postuler</a>
+                        <a href="{event.fb_even}" target="_blank" class="button" style="background-color: #053682; color:white">Postuler</a>
                     {:else if event.site}   <!-- Site Web ou page Facebook événement -->
-                        <a href="{event.site}" class="button" style="background-color: #053682; color:white">Postuler</a>
+                        <a href="{event.site}" target="_blank" class="button" style="background-color: #053682; color:white">Postuler</a>
                     {:else}     <!-- Aucune information -->
                         <p>Méthode inconnue</p>
                     {/if}
