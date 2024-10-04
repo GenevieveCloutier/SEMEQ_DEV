4e5b2f2d45a4ac67eb350a9c96cba23db3232819
Aurore Langlet <aurore_langlet@etik.com>
2024-10-03 20:51:22 -0400

    En cours de tests

--- src/routes/appels_candidatures/+page.server.js
+++ src/routes/appels_candidatures/+page.server.js
@@ -1,13 +1,20 @@
 import { findAll } from "$lib/db/controllers/Evenements.controller.js";
+import { findAll as findVilles, findOne as findNomVille } from "$lib/db/controllers/Villes.controller.js";
 
 /**
- * Charge les détails des événements dont la date d'aujourd'hui est comprise entre la date de début et
+ * Charge les détails des événements approuvés dont la date d'aujourd'hui est comprise entre la date de début et
  * celle de fin des appels de candidatures des événements du répertoire.
  * @param {Object} params - Les paramètres de la requête.
  * @returns {Object} - Les événements et leurs détails pour ceux ayant des appels de candidatures en cours
  *                     (si utilisateur exposant abonné connecté).
  */
-export async function load({params, /*cookies*/}) {
+export async function load({ params }){
     const events = await findAll();
+    const villes = await findVilles();
+    let ville = null;
+    for (const event of villes) {
+        ville = await findNomVille({id: event.ville_id});
+        event.ville = ville;
+    }
     return { events:events }
 }
--- src/routes/appels_candidatures/+page.svelte
+++ src/routes/appels_candidatures/+page.svelte
@@ -28,20 +28,31 @@
         <tbody>
             {#each events as event}
             <tr>
-                <td>{event.nom}</td>
+                <td>{event.nom || "Inconnu"}</td>
                 <td></td>
-                <td>{event.ville_id}</td>
-                <td>{event.debut_even}</td>
-                <td>{event.fin_even}</td>
+                <td>{event.ville.nom || "Inconnu"}</td>
+                <td>{event.debut_even || "Inconnu"}</td>
+                <td>{event.fin_even || "Inconnu"}</td>
                 <td>
-                    {#if event.form_cand}
-                    <a href="{event.form_cand}" class="button">Postuler</a>
-                    {:else}
-                    <a href="{event.courriel}" class="button">Postuler</a>
+                    {#if event.form_cand}   <!-- Formulaire appel de candidatures -->
+                        <a href="{event.form_cand}" class="button" style="background-color: #053682; color:white">Postuler</a>
+                    {:else if event.courriel}   <!-- Courriel pour information ou inscription -->
+                        <a href="mailto:{event.courriel}" class="button" style="background-color: #053682; color:white">Postuler</a>
+                    {:else if event.fb_even}    <!-- Événement Facebook -->
+                        <a href="{event.fb_even}" class="button" style="background-color: #053682; color:white">Postuler</a>
+                    {:else if event.site}   <!-- Site Web ou page Facebook événement -->
+                        <a href="{event.site}" class="button" style="background-color: #053682; color:white">Postuler</a>
+                    {:else}     <!-- Aucune information -->
+                        <p>Méthode inconnue</p>
                     {/if}
                 </td>
-                <td>{event.fin_cand}</td>
+                <td>{event.fin_cand || "Inconnu"}</td>
             </tr>
+            {:else}
+                <section class="section">
+                    <h1 class="title"> Désolé  <span class="icon is-large"><i class="fa-regular fa-face-sad-tear fa-xl"></i></span></h1>
+                    <h2 class="subtitle">Il n'y a aucun appel de candidature en cours.</h2>
+                </section>
             {/each}
         </tbody>
     </table>
