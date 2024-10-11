06e8929232ce702bcfd80ea28426be6cc72b4bee
MapleCialys <160977634+MapleCialys@users.noreply.github.com>
2024-10-10 20:10:43 -0400

    debut de modif

--- src/routes/gestionnaire/+layout.svelte
+++ src/routes/gestionnaire/+layout.svelte
@@ -22,22 +22,22 @@
       <aside class="menu">
         <p class="menu-label" id="general-label">Général</p>
         <ul class="menu-list is-hidden-mobile" id="general-menu">
-          <li><a href="/admin" id="dashboard">Dashboard</a></li>
-          <li><a href="/admin/utilisateurs" id="utilisateurs">Utilisateurs</a></li>
-          <li><a href="/admin/blogue" id="blogue">Blogue</a></li>
+          <li><a href="/gestionnaire" id="dashboard">Dashboard</a></li>
+          <li><a href="/gestionnaire/utilisateurs" id="utilisateurs">Utilisateurs</a></li>
+          <li><a href="/gestionnaire/blogue" id="blogue">Blogue</a></li>
         </ul>
         <p class="menu-label" id="events-label">Evénements</p>
         <ul class="menu-list is-hidden-mobile" id="events-menu">
-          <li><a href="/admin/evenements" id="evenements">Liste des événements</a></li>
-          <li><a href="/admin/evenements/nouveau" id="evenementsnouveau">Ajouter un événement</a></li>
-          <li><a href="/admin/approbations" id="approbation">Approbation en attente</a></li>
+          <li><a href="/gestionnaire/evenements" id="evenements">Liste des événements</a></li>
+          <li><a href="/gestionnaire/evenements/nouveau" id="evenementsnouveau">Ajouter un événement</a></li>
+          <li><a href="/gestionnaire/approbations" id="approbation">Approbation en attente</a></li>
         </ul>
         <p class="menu-label" id="boutique-label">Boutique</p>
         <ul class="menu-list is-hidden-mobile" id="boutique-menu">
-          <li><a href="/admin/boutique" id="boutique">Liste d'article</a></li>
-          <li><a href="/admin/boutique/nouveau" id="boutiquenouveau">Ajouter un article</a></li>
-          <li><a href="/admin/code_promo" id="code_promo">Liste des codes promos</a></li>
-          <li><a href="/admin/code_promo/nouveau" id="code_promonouveau">Ajouter un code promo</a></li>
+          <li><a href="/gestionnaire/boutique" id="boutique">Liste d'article</a></li>
+          <li><a href="/gestionnaire/boutique/nouveau" id="boutiquenouveau">Ajouter un article</a></li>
+          <li><a href="/gestionnaire/codes_promo" id="code_promo">Liste des codes promos</a></li>
+          <li><a href="/gestionnaire/codes_promo/nouveau" id="code_promonouveau">Ajouter un code promo</a></li>
         </ul>
       </aside>
     </div>
