8721022f9f1165187f49e156479193604f590469
MapleCialys <160977634+MapleCialys@users.noreply.github.com>
2024-10-13 14:46:28 -0400

    jsDoc bouton load

--- src/lib/components/formulaires/submitButon.svelte
+++ src/lib/components/formulaires/submitButon.svelte
@@ -2,12 +2,21 @@
 	import { erreur } from "../../outils/formHandlers";
 
 	export let texte;
-	export let fonction = () => {};
+	export let fonction = () => {}; //Vraiment besoin ? ou on supprime ?
 
+	/**
+ 	* Ajoute la classe 'is-loading' à l'élément déclencheur de l'événement.
+ 	*
+ 	* Cette fonction permet d'ajouter une classe CSS au bouton de validation
+ 	* afin de signaler visuellement que le chargement est en cours.
+ 	*
+ 	* @param {Event} event - L'événement déclenché, contenant l'élément cible.
+ 	*/
 	function chargement(event){
 		event.target.classList.add('is-loading');
 	}
 
+	//Permet de retirer la classe de chargement si un message d'erreur s'affiche
 	$: if ($erreur) {
 		document.getElementById('submitButton').classList.remove('is-loading');
     }
