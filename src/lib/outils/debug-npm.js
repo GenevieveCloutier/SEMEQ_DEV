435609e4102c5a53abcbe92fa906fd15135c6d6b
MapleCialys <160977634+MapleCialys@users.noreply.github.com>
2024-10-14 18:44:16 -0400

    oups, oublie nul

--- src/lib/outils/debug-npm.js
+++ src/lib/outils/debug-npm.js
@@ -1,7 +1,5 @@
-import { log } from 'console';
 import fs from 'fs';
 
-// Chemin du fichier à modifier
 const fichier = 'src/lib/outils/debug.js';
 const argv = process.argv.slice(2);
 const option = argv[0] === '-on' ? 'true' : argv[0] === '-off' ? 'false' : 'false';
@@ -9,9 +7,9 @@ const option = argv[0] === '-on' ? 'true' : argv[0] === '-off' ? 'false' : 'fals
 try {
     let contenu = await fs.promises.readFile(fichier, 'utf-8');
     if (argv[0] === '-on')
-        contenu = contenu.replace(/export let DEBUG_MODE = false;/g, "export let DEBUG_MODE = true;");
+        contenu = contenu.replace(/const DEBUG_MODE = false;/g, "const DEBUG_MODE = true;");
     else if (argv[0] === '-off')
-        contenu = contenu.replace(/export let DEBUG_MODE = true;/g, "export let DEBUG_MODE = false;");
+        contenu = contenu.replace(/const DEBUG_MODE = true;/g, "const DEBUG_MODE = false;");
     else
         console.log('Option non reconnue.\n-- -on pour activer\n-- -off pour désactiver.');
         
