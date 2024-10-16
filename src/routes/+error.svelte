35389bbf9b50e45c3eb3689f64652c70e4de26df
Aurore Langlet <aurore_langlet@etik.com>
2024-10-16 12:53:44 -0400

    Ajout error 403 accessForbidden

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,7 @@
   import { page } from '$app/stores';
   let isNotFound = $page.status === 404;
   let serverProblem = $page.status === 500;
+  let accessForbidden = $page.status === 403;
 </script>
 
 {#if isNotFound}
@@ -11,6 +12,9 @@
 {:else if serverProblem}
   <h1 class="title has-text-centered">Erreur {$page.status}</h1>
   <h2 class="subtitle has-text-centered">Ce n'est pas vous, c'est notre serveur qui a de la difficulté</h2>
+{:else if accessForbidden}
+  <h1 class="title has-text-centered">Erreur {$page.status}</h1>
+  <h2 class="subtitle has-text-centered">{$page.error?.message}</h2>
 {:else}
   <h1 class="title has-text-centered">Erreur {$page.status}: {$page.error?.message}</h1>
 {/if}
