3b4c0963587f19a3ed27d3c07d93d025c5d58c59
Aurore Langlet <aurore_langlet@etik.com>
2024-10-15 20:22:05 -0400

    Retirer delete notif

--- src/lib/components/notifications/notifDanger.svelte
+++ src/lib/components/notifications/notifDanger.svelte
@@ -1,19 +1,10 @@
 <script>
   import { erreur } from "../../outils/formHandlers";
-  
-
-  function supprimerNotification(event) {
-    event.target.parentNode.remove();
-  }
-  $: if ($erreur) {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
 </script>
 
 <div class="block">
     {#if $erreur}
     <div class="notification is-danger has-text-centered">
-        <button class="delete" on:click={supprimerNotification}></button>
         <p>{$erreur}</p>
     </div>
     {/if}
