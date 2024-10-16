c08c8fa24f848f2488a42ce4eb030095e26d7830
Aurore Langlet <aurore_langlet@etik.com>
2024-10-15 20:32:53 -0400

    Backend forms comptes payants

--- src/routes/creation_compte/+page.server.js
+++ src/routes/creation_compte/+page.server.js
@@ -1,7 +1,14 @@
+import { findAll } from '$lib/db/controllers/Villes.controller.js'; "$lib/db/controllers/Utilisateurs.controller";
+import { redirect } from '@sveltejs/kit';
 
-export async function load({ cookies, parent }){
-    const { cookiesAll } = await parent();
+export async function load({cookies}){
+    const villes = await findAll();   
+
+    // Aller chercher tous les utilisateurs de la BD
+    const users = await findAll();
+    const session = cookies.get('session');
+    const role = cookies.get('role');
+
     
-    return ({cookiesAll: cookiesAll})
-    
+    return {users: users, session: session, role:role, villes:villes};
 }
