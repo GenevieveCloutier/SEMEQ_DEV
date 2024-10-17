fea2d6a3d2fb3ed6c0b210f297308d7764ad1a50
Aurore Langlet <aurore_langlet@etik.com>
2024-10-16 21:33:46 -0400

    Page politiques vente

--- src/lib/components/formulaires/checkboxConditionsVente.svelte
+++ src/lib/components/formulaires/checkboxConditionsVente.svelte
@@ -2,7 +2,7 @@
     <div class="control">
       <label class="checkbox">
         <input type="checkbox" class="exclus" id="conditions_vente" required>
-        J'ai lu et j'accepte les <a href="#">conditions de vente de services</a>.<span class="rouge">*</span>
+        J'ai lu et j'accepte les <a href="/politiques_vente">conditions de vente de services</a>.<span class="rouge"> *</span>
       </label>
     </div>
 </div>
