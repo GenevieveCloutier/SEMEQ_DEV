bb670a5d67518c5bb8c5c467fe191055498a67f2
Aurore Langlet <aurore_langlet@etik.com>
2024-10-17 22:29:47 -0400

    Ajout findOne()

--- src/lib/db/controllers/Produits.controller.js
+++ src/lib/db/controllers/Produits.controller.js
@@ -1,5 +1,6 @@
 import { Produit } from "../models/Produit.model.js";
 import { Type } from "../models/Type.model.js";
+import { error } from "@sveltejs/kit";
 
 /**
  * Récupère tous les produits de la base de données en incluant les informations de leur type.
@@ -26,3 +27,33 @@ export async function findAll(){
         throw error;
     });
 };
+
+/**
+ * Trouve un produit selon les critères de recherche spécifiés.
+ *
+ * Cette fonction cherche un produit en fonction des critères passés dans `p_where`.
+ * Elle inclut également les informations du type associé au produit, si disponible.
+ *
+ * @param {Object} p_where - Critères de recherche pour trouver un produit.
+ *
+ * @return {Object|null} res - L'objet produit avec son type associé, ou `null` si aucun produit n'est trouvé.
+ *
+ * @throws {Error} - Lance une erreur en cas de problème lors de la recherche.
+ */
+export async function findOne(p_where){
+    return await Produit.findOne({ where: p_where, include: [{
+        model: Type,
+        as: 'type'
+    }]})
+    .then(res => {
+        if(res)
+        return {
+            ...res.dataValues,
+            role: res.type ? res.type.dataValues : null
+        };
+        else
+            return null;
+    }).catch((error) => {;
+        throw error;
+    });
+}
