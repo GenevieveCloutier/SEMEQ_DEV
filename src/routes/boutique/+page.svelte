f9ff48141ef607d49d2ca57dfcca0a81ca046af9
Aurore Langlet <aurore_langlet@etik.com>
2024-10-18 21:54:51 -0400

    Filtrer produits selon leur type

--- src/routes/boutique/+page.svelte
+++ src/routes/boutique/+page.svelte
@@ -1,11 +1,17 @@
 <script>
     import H1Title from "$lib/components/titres/h1Title.svelte";
+    import H2Title from "$lib/components/titres/h2Title.svelte";
     import BoutonGris from "$lib/components/boutons/boutonGris.svelte";
     import Recherche from '$lib/components/generaux/recherche.svelte';
     import RechercheNoResult from '$lib/components/generaux/rechercheNoResult.svelte';
 
     export let data;
     const produits = data.produits;
+
+    // Filtrer produits selon leur type
+    const formations = produits.filter(produit => produit.type.nom === 'formation');
+    const outils = produits.filter(produit => produit.type.nom === 'outil');
+    const abonnements = produits.filter(produit => produit.type.nom === 'abonnement');
 
     // Barre de recherche
     let searchQuery = '';
@@ -17,15 +23,24 @@
 
 <Recherche bind:searchQuery typeRecherche="une formation, un outil ou un abonnement" />
 
-<div>
-    {#if produits.filter(produit => produit.nom.toLowerCase().includes(searchQuery.toLowerCase())).length === 0}
-        <RechercheNoResult />
-    {:else}
-    {#each produits.filter(produit => produit.nom.toLowerCase().includes(searchQuery.toLowerCase())) as produit}
-        <li>{produit.nom} {produit.type.nom}</li>
+{#if produits.filter(produit => produit.nom.toLowerCase().includes(searchQuery.toLowerCase())).length === 0}
+    <RechercheNoResult />
+{:else}
+    <H2Title title={"Formations"} />
+    {#each formations.filter(produit => produit.nom.toLowerCase().includes(searchQuery.toLowerCase())) as formation}
+        <li>{formation.nom}</li>
     {/each}
-    {/if}
-</div>
+
+    <H2Title title={"Outils et ressources"} />
+    {#each outils.filter(produit => produit.nom.toLowerCase().includes(searchQuery.toLowerCase())) as outil}
+        <li>{outil.nom}</li>
+    {/each}
+
+    <H2Title title={"Abonnements"} />
+    {#each abonnements.filter(produit => produit.nom.toLowerCase().includes(searchQuery.toLowerCase())) as abonnement}
+        <li>{abonnement.nom}</li>
+    {/each}
+{/if}
 
 <br><br>
 <BoutonGris lien={"/creation_compte/exposant"} texte={"Acheter un abonnement exposant"}/>
