0c9695591efcc844973a1acc2c62a94e75b452fd
Aurore Langlet <aurore_langlet@etik.com>
2024-10-23 15:14:48 -0400

    Mise en forme pour correspondre à boutonGris

--- src/lib/components/generaux/retour.svelte
+++ src/lib/components/generaux/retour.svelte
@@ -1,2 +1,9 @@
+<a href="javascript:window.history.back();"><button class="px-3 py-3"> Retour </button></a>
 
-<a href="javascript:window.history.back();" class="button is-light">Retour</a>
+<style>
+	button {
+		background-color: #d9d9d9;
+		border-radius: 5px;
+		color: black;
+	}
+</style>
