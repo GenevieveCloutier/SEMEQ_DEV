73a14b16b9a65fe982e912e09815577152aaddfd
Aurore Langlet <aurore_langlet@etik.com>
2024-11-14 22:49:36 -0500

    Page paiement 1er jet

--- src/routes/panier/paiement/+page.svelte
+++ src/routes/panier/paiement/+page.svelte
@@ -1,12 +1,18 @@
 
 <script>
     import H2Title from "$lib/components/titres/h2Title.svelte";
+    import Retour from "$lib/components/generaux/retour.svelte";
     import { loadScript } from "@paypal/paypal-js";
 	  import { onMount } from "svelte";
 
     export let data;
     const paniers = data.paniers;
     const utilisateur = data.utilisateur;
+
+    // Calcul économie
+    let economie = paniers.reduce((acc, panier) => {
+        return acc + (panier.produit.prix_v - panier.produit.prix_a);
+    }, 0);
 
     export let total = 10;
     //<!--! La pour le test j'ai mis la clé directement ici, mais va falloir la mettre en dotenv
@@ -48,24 +54,95 @@
 });
 </script>
 
-<div class="box">
+<!-- Afficher la barre de suivi étape "Paiement" -->
+
+<div class="container is-narrow box">
 
   <H2Title title={"Récapitulatif de commande"} />
 
-  <div class="content">
-    <b>Liste d'items:</b>
-    <ul>
-        {#each paniers as panier}
-        <li>{panier.produit.nom}</li>
-        {/each}
-    </ul>
-  </div>
+  <div class="container-centered block">
+      <table class="table is-narrow no-border-table">
+        <thead>
+          <tr>
+            <th>Liste d'items :</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each paniers as panier}
+          <tr>
+            <td>{panier.produit.nom}</td>
+            <td >{panier.produit.type.nom}</td>
+            {#if utilisateur.abonne === true}
+                  <td class="has-text-right">{panier.produit.prix_a === 0 ? "Gratuit" : `${panier.produit.prix_a.toFixed(2)} $`}</td>
+              {:else}
+                  <td class="has-text-right">{panier.produit.prix_v === 0 ? "Gratuit" : `${panier.produit.prix_v.toFixed(2)} $`}</td>
+            {/if}
+          </tr>
+          {/each}
+        </tbody>
+      </table>
+      
+      <form class="block is-narrow">
+        <label class="label" for="code_promo">Tu as un code promo?</label>
+        <div class="field has-addons">
+            <p class="control">
+                <input class="input" name="code_promo" type="text" placeholder="Code promo">
+            </p>
+            <p class="control">
+                <button type="submit" class="button" id="btn_code_promo">Appliquer</button>
+            </p>
+        </div>
+      </form>
 
-  <!--! La div pour le bouton paypal  -->
-  <div id="paypal-button-container"></div>
+      <div class="block is-narrow has-text-right">
+        {#if economie !== 0} <!-- Afficher s'il y a une économie -->
+            {#if utilisateur.abonne === true}
+            <p>En étant abonné, tu économises <b>{`${economie.toFixed(2)} $`}</b> sur ta commande!<br></p>
+            {:else}
+            <p>En étant abonné, tu économiserais <b>{`${economie.toFixed(2)} $`}</b> en plus de profiter de plusieurs avantages exclusifs!<br></p>
+            {/if}
+        {/if}
+        
+        <div class="columns">
+          <div class="column has-text-right">
+              <b>Total :</b>
+          </div>
+          <div class="column is-narrow has-text-right">
+              <b>{total === 0 ? "Gratuit" : `${total.toFixed(2)} $`}</b>
+          </div>
+        </div>
+      </div>
+
+  </div>
+  
+  <!-- La div pour le bouton paypal -->
+  <div class="container is-flex is-justify-content-center is-align-items-center">
+    <div id="paypal-button-container"></div>
+  </div>
 
 </div>
 
+<div class="block has-text-right">
+  <Retour />
+</div>
 
-  
+<style>
+  .container-centered {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    /*background-color: green;*/
+  }
+  .no-border-table ,
+  .no-border-table th,
+  .no-border-table td {
+    border: none;
+  }
 
+  #paypal-button-container {
+    width: 50%;
+  }
+</style>
