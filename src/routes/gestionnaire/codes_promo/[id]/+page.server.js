ba99d31b64d530255bef1225eee19be87c067bd2
Aurore Langlet <aurore_langlet@etik.com>
2024-11-19 23:03:18 -0500

    Correctif affichage date

--- src/routes/gestionnaire/codes_promo/[id]/+page.server.js
+++ src/routes/gestionnaire/codes_promo/[id]/+page.server.js
@@ -2,5 +2,8 @@ import { findOne } from '../../../../lib/db/controllers/Partenaires.controller';
 
 export async function load({cookies, params}) {
     const code = await findOne({id: params.id});
+    if (code.expiration) {
+        code.expiration = code.expiration.toLocaleDateString('fr-CA');
+    }
     return {code};
 }
