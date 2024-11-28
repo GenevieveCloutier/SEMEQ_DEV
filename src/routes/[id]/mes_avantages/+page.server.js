e1aeffd6834652587c3e2e01daff6b9b90ca92cb
Aurore Langlet <aurore_langlet@etik.com>
2024-11-28 16:53:47 -0500

    Acces admin

--- src/routes/[id]/mes_avantages/+page.server.js
+++ src/routes/[id]/mes_avantages/+page.server.js
@@ -8,14 +8,15 @@ export async function load({ cookies, params }){
     const cookiesId = cookies.get('id');
     const user = await findOne({ id: cookiesId });
     let abonne = user.abonne;
+    const role = cookies.get('role');
 
     // Restriction accès page aux abonnés
-    if (abonne !== true) {
+    if (!abonne && role !== '1') {
         throw error(403, 'Seuls les abonnés peuvent accéder à cette page.');
     }
 
     // Date du jour au format ISO avec l'heure 00:00:00 +00:00 pour comparer avec dates dans BD
-    let aujourdhui = new Date().toISOString().split("T")[0]  + " 00:00:00.000 +00:00";
+    let aujourdhui = new Date().toISOString().split("T")[0]  + " 00:00:00.000 +00:00"; 
 
     let partenaires = await Partenaire.findAll({
         order: [
@@ -34,8 +35,7 @@ export async function load({ cookies, params }){
 
     let resultat = partenaires.map(partenaire => ({
         ...partenaire.dataValues,
-        expiration: partenaire.expiration === null ? "Aucune"
-            : partenaire.expiration.toLocaleDateString('fr-CA', {timeZone: 'UTC'}),
+        expiration: partenaire.expiration === null ? "Aucune" : `${partenaire.expiration.toLocaleDateString('fr-CA', {timeZone: 'UTC'})}`,
         categorie: partenaire.categorie ? partenaire.categorie.dataValues : null,
     }));
 
