afe75ca564132874a5d58ed7c65767703e4413ae
Aurore Langlet <aurore_langlet@etik.com>
2024-11-28 20:49:40 -0500

    Ajouts champs, rabais, categorie_id, produit_id et type_id

--- src/routes/gestionnaire/codes_promo/nouveau/+page.svelte
+++ src/routes/gestionnaire/codes_promo/nouveau/+page.svelte
@@ -7,7 +7,9 @@
 	import { creationCodePromo } from '$lib/outils/formHandlers';
 	
 	export let data;
-	const { categories } = data;
+	const categories = data.categories;
+	const produits = data.produits;
+	const types = data.types;
 
     // Mettre en évidence le lien actif dans menu latéral gestionnaire
     import { onMount } from "svelte";
@@ -40,17 +42,37 @@
 				</div>
 			</div>
 
-            <div class="field">
-				<label class="label" for="code">Code <span class="rouge">*</span></label>
-				<div class="control">
-					<input
-						class="input"
-						type="text"
-						name="code"
-						id="code"
-						placeholder="Code"
-                        required
-					/>
+			<div class="columns">
+				<div class="column is-half">
+					<div class="field">
+						<label class="label" for="code">Code <span class="rouge">*</span></label>
+						<div class="control">
+							<input
+								class="input"
+								type="text"
+								name="code"
+								id="code"
+								placeholder="Code"
+								required
+							/>
+						</div>
+					</div>
+				</div>
+
+				<div class="column is-half">
+					<div class="field">
+						<label class="label" for="rabais">Rabais</label>
+						<div class="control">
+							<input
+								class="input"
+								type="text"
+								name="rabais"
+								id="rabais"
+								placeholder="% de rabais"
+								required
+							/>
+						</div>
+					</div>
 				</div>
 			</div>
 
@@ -60,31 +82,64 @@
                         class="textarea"
                         name="avantage"
                         id="avantage"
-                        placeholder="Description de l'avantage"
+                        placeholder="Description de l'avantage (ne pas ré-inscrire le % de rabais)"
                         rows="5"
                         required
 				></textarea>
             </div>
 
-			<div class="field">
-				<label for="categorie_id" class="label">Catégorie <span class="rouge">*</span></label>
-				<div class="control">
-					<div class="select is-fullwidth">
-						<select name="categorie_id" id="type" required>
-							<option  disabled selected>Choisissez une catégorie de partenaire</option>
-							{#each categories as categorie}
-								<option value={categorie.id}>{categorie.nom}</option>
-							{/each}
-						</select>
+			<div class="columns">
+				<div class="column is-half">
+					<div class="field">
+						<label for="categorie_id" class="label">Catégorie de partenaire <span class="rouge">*</span></label>
+						<div class="control">
+							<div class="select is-fullwidth">
+								<select name="categorie_id" id="categorie_id" required>
+									<option  disabled selected>Choisissez une catégorie de partenaire</option>
+									{#each categories as categorie}
+										<option value={categorie.id}>{categorie.nom}</option>
+									{/each}
+								</select>
+							</div>
+						</div>
+					</div>
+
+					<div class="field">
+						<label for="produit_id" class="label">Produit de la boutique SÉMEQ</label>
+						<div class="control">
+							<div class="select is-fullwidth">
+								<select name="produit_id" id="produit_id" required>
+									<option  disabled selected>Choisissez un produit sur lequel appliquer le rabais</option>
+									{#each produits as produit}
+										<option value={produit.id}>{produit.nom}</option>
+									{/each}
+								</select>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-
-            <div class="field">
-                <label class="label" for="expiration">Date d'expiration</label>
-					<div class="control">
-						<input class="input" type="date" name="expiration" id="expiration" />
+				<div class="column is-half">
+					<div class="field">
+						<label class="label" for="expiration">Date d'expiration</label>
+							<div class="control">
+								<input class="input" type="date" name="expiration" id="expiration" />
+							</div>
 					</div>
+
+					<div class="field">
+						<label for="categorie_id" class="label">Types de produits de la boutique SÉMEQ</label>
+						<div class="control">
+							<div class="select is-fullwidth">
+								<select name="categorie_id" id="categorie_id">
+									<option  disabled selected>Choisissez un type de produit sur lequel appliquer le rabais</option>
+									{#each types as type}
+										<option value={type.id}>{type.nom}</option>
+									{/each}
+								</select>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 
             <div class="field">
