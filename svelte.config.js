4febb68449fa3e267b5c31abd41bcebae911a543
Genevieve CLoutier <shail1234@hotmail.com>
2024-12-06 09:46:37 -0500

    string pour adapter

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 		alias:{
 			$api: 'src/routes/api',
 			$routes: 'src/routes',
-			adapter: adapter()
+			adapter: 'adapter()'
 		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
