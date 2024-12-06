ed164e83c75b22318e51ef80e4353e49bc971121
Genevieve CLoutier <shail1234@hotmail.com>
2024-12-05 19:20:19 -0500

    correction ../lib

--- src/routes/deconnexion/+page.server.js
+++ src/routes/deconnexion/+page.server.js
@@ -1,5 +1,5 @@
 import { fail } from '@sveltejs/kit';
-import { deleteCookie } from '../../lib/db/controllers/sessions.controller';
+import { deleteCookie } from '$lib/db/controllers/sessions.controller';
 import { redirect } from '@sveltejs/kit';
 
 export async function load({ cookies })
